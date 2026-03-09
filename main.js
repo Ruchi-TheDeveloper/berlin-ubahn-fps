da08bc114374bb357126ab2849c54dad26f1208b
Ruchi-TheDeveloper <165423439+Ruchi-TheDeveloper@users.noreply.github.com>
2026-03-09 17:11:37 +0530

    Update THREE.js imports to use CDN links

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 
-import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
+import { PointerLockControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/PointerLockControls.js';
 
 let camera, scene, renderer, controls;
 let velocity = new THREE.Vector3();
